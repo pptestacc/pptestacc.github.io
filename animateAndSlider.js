7736843fe9c30e6d6af6ef0042c683ca452ba957
ppanayiotou2 <33517436+ppanayiotou2@users.noreply.github.com>
2017-12-15 19:10:31 +0200

    s
    sfsd

--- animateAndSlider.js
+++ animateAndSlider.js
@@ -1,9 +1,9 @@
  /* 
 
- SCRIPT FOR ACTIVATING ANIMATIONS ACCRORDING TO THE VIEWPORT POSITION
- + STOPS INFINITE SLIDER MOVEMENT ON USER HOVER
-  
- */
+   SCRIPT FOR ACTIVATING ANIMATIONS ACCRORDING TO THE VIEWPORT POSITION
+   + STOPS INFINITE SLIDER MOVEMENT ON USER HOVER
+    
+   */
 
 
  $(document).ready(function () {
@@ -14,9 +14,10 @@
      });
 
      $('.frame *').addClass("hidden").viewportChecker({
-         classToAdd: 'visible animated zoomIn',
+         classToAdd: 'visible animated fadeInLeft',
          offset: -500
      });
+
 
      if ((screen.width > 950)) {
          $(".photo-roll img").hover(function () {
@@ -37,10 +38,10 @@
          classToAdd: 'visible animated fadeInDown',
          offset: 100
      });
-     
+
      $('.even').addClass("hidden").viewportChecker({
          classToAdd: 'visible animated fadeInUp',
          offset: 100
      });
-     
+
  });
